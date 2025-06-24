a7e2313234b50213dddf6d8d45afb828b79e38f0
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-06-24 09:35:10 -0500

    .

--- tests/pico/test.c
+++ tests/pico/test.c
@@ -1,6 +1,15 @@
+#include <stdio.h>
+
 #include "pico/stdlib.h"
 #include "pico/time.h"
 #include "hardware/spi.h"
+
+#include <i2c_fifo.h>
+#include <i2c_slave.h>
+
+static uint8_t next = 0xfe;
+
+static const uint I2C_SLAVE_ADDRESS = 0x27;
 
 // Pico W devices use a GPIO on the WIFI chip for the LED,
 // so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
@@ -46,14 +55,11 @@ static bool update_led_cb(repeating_timer_t* rt)
 
 static void recv_spi()
 {
-    static uint8_t next = 0xff;
-
-    while (spi_is_readable(spi1)) {
-        while (!spi_is_writable(spi1)) {}
-        uint8_t r;
-        spi_write_read_blocking(spi1, &next, &r, 1);
-        next = r + 1;
-    }
+    uint8_t r;
+    spi_read_blocking(spi1, 0, &r, 1);
+    printf("> %02X\n", &r);
+    next = r + 1;
+    spi_write_blocking(spi1, &next, 1);
 }
 
 static void spi_slave_init()
@@ -66,21 +72,50 @@ static void spi_slave_init()
     gpio_set_function(12, GPIO_FUNC_SPI);
     gpio_set_function(13, GPIO_FUNC_SPI);
 
+    spi1_hw->imsc = SPI_SSPIMSC_RTIM_BITS | SPI_SSPIMSC_RXIM_BITS;
+
     irq_set_enabled(SPI1_IRQ, true);
     irq_set_exclusive_handler(SPI1_IRQ, recv_spi);
-    spi1_hw->imsc = SPI_SSPIMSC_RTIM_BITS | SPI_SSPIMSC_RXIM_BITS;
 }
 
 
-int main() {
+static void i2c_handle(i2c_inst_t* i2c, i2c_slave_event_t event)
+{
+    printf("I2C\n");
+}
+
+
+static void setup_slave()
+{
+    // https://github.com/vmilea/pico_i2c_slave/blob/master/example_mem/example_mem.c
+    gpio_init(4);
+    gpio_set_function(4, GPIO_FUNC_I2C);
+    gpio_pull_up(4);
+
+    gpio_init(5);
+    gpio_set_function(5, GPIO_FUNC_I2C);
+    gpio_pull_up(5);
+
+    i2c_init(i2c0, 100000);
+    i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, i2c_handle);
+}
+
+
+int main()
+{
+    stdio_uart_init();
+
     int rc = pico_led_init();
     hard_assert(rc == PICO_OK);
 
     spi_slave_init();
+    setup_slave();
 
     repeating_timer_t timer;
     add_repeating_timer_ms(LED_DELAY_MS, update_led_cb, NULL, &timer);
 
     while (true) {
+        printf("Hello world\n");
+        sleep_ms(1000);
     }
 }
