f7e7a6242b9f04d5c7aef6f6b5ec0346eff160e3
Andre Wagner <WagnerAndre@JohnDeere.com>
2025-06-25 13:02:21 -0500

    .

---
+++ tests/atmega328/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+
+#include "uart.h"
+
+int main(void)
+{
+    uart_init();
+    for (;;) {
+        puts_P(PSTR("Hello world!"));
+        _delay_ms(500);
+    }
+    return 0;
+}
