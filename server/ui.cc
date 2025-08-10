d608c4ac76fb1a76fb05efe0f37ed982ae3bcb66
Andre Wagner <andre.nho@gmail.com>
2025-08-10 06:42:01 -0500

    .

--- server/ui.cc
+++ server/ui.cc
@@ -6,23 +6,55 @@
 
 extern "C" {
 #include "ssd1306-driver/ssd1306.h"
-#include "ssd1306-driver/toshiba-font.h"
+#include "ssd1306-driver/toshiba_font.h"
 }
 
 #define BEEP 16
+
+static int H;  // handle
+static SSD_Buffer* bf;
+               
+static void i2c_init(void* data)
+{
+    int bus = (int) data;
+    H = i2cOpen(bus, 0x3c, 0);
+}
+
+static void i2c_finalize(void* data)
+{
+    (void) data;
+    i2cClose(H);
+}
+
+static void i2c_send_bytes(void* data, uint8_t const* bytes, size_t sz)
+{
+    (void) data;
+    i2cWriteDevice(H, (char *) bytes, sz);
+
+    /*
+    for (size_t i = 0; i < sz; ++i)
+        printf("%02X ", bytes[i]);
+    printf("\n");
+    */
+}
 
 namespace ui {
 
 static void init_display()
 {
-    // TODO
+    ssd1306_init((I2CFunctions) {
+        .data = (void *) 1,
+        .init = i2c_init,
+        .send_bytes = i2c_send_bytes,
+        .finalize = i2c_finalize,
+    }, 32);
+
+    bf = ssd1306_create_buffer();
 }
 
 void init()
 {
     gpioSetMode(BEEP, PI_OUTPUT);
-    // TODO - init pins
-
     init_display();
 }
 
